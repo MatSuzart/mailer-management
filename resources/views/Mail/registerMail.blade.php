760314c5ccf9c155d29647d5424a0d6fc563221e
MatheusSuzart <mathsuzart@outlook.com>
2023-01-14 23:58:13 -0300

    email-html-layout

--- resources/views/Mail/registerMail.blade.php
+++ resources/views/Mail/registerMail.blade.php
@@ -1,3 +1,15 @@
-<div style="display:flex; justify-content: center">
-<h1 style="color:red;"> hello world</h1>
-</div>
+<html>
+    <head>
+        <meta charset="en">
+        <meta name="viewport" content="width-device-width, initial-scale=1.0">
+        <meta http-esquiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+        <meta name="description" content="E-MAIL DESCRIPTION">
+    </head>
+    <body>
+        <div style="dispaly: flex; justify-content: center">
+            <h1 style="color:red;"> SEJA BEM VINDO, {{$NOME}}</h1>
+            <h1></h1>
+        </div>
+    </body>
+</html>
